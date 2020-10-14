a2d8025d57fd15e88b187c70561d520e11631a16
AdrianoLima19 <surerloki3379@gmail.com>
2020-10-14 13:06:28 -0300

    Refactoring all Router methods and classes.
    Refactored router methods.
    Refactored how the request is handled.
    Refactored how the router will response the given request.
    Refactored how the router will handle any response or request error.

---
+++ demo/Web.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace SurerLoki\Router\Demo;
+
+class Web
+{
+    public function home($data)
+    {
+        echo "<h1>Web home</h1>";
+
+        var_dump(
+            $data
+        );
+    }
+    public function user($data)
+    {
+        echo "<h1>Web user</h1>";
+
+        var_dump(
+            $data
+        );
+    }
+    public function changeUser($data)
+    {
+        echo "<h1>Web user</h1>";
+
+        var_dump(
+            $data
+        );
+    }
+    public function admin($data)
+    {
+        echo "<h1>Web Admin</h1>";
+
+        var_dump(
+            $data
+        );
+    }
+}
--- src/Dispatch.php
+++ src/Dispatch.php
@@ -2,109 +2,133 @@
 
 namespace SurerLoki\Router;
 
-abstract class Dispatch
+class Dispatch
 {
-    public function run()
+    private $fallback;
+    private $error;
+
+    public function execute()
     {
         if (empty($this->routes)) {
+
             $this->error = Router::NOT_FOUND;
-            return;
-        }
 
-        if (empty($this->routes[$this->httpMethod])) {
-            $this->error = Router::METHOD_NOT_ALLOWED;
-            return;
-        }
-
-        foreach ($this->routes[$this->httpMethod] as $route => $params) {
-            if (preg_match("~^" . trim($route, '/') . "$~", $this->requestRoute)) {
-                $runRoute = $params;
+            if (!empty($this->fallback)) {
+                $this->runFallback();
             }
+            return;
+        }
+
+        foreach ($this->routes as $method => $routes) {
+            foreach ($routes as $route => $value) {
+                $route = trim($route, '/');
+
+                if (preg_match("~^" . $route . "$~", $this->request->getUri()) && $value['method'] == $this->request->getHttpMethod()) {
+                    $runRoute = $value;
+                    break;
+                } elseif (preg_match("~^" . $route . "$~", $this->request->getUri())) {
+                    $routeExists = true;
+                }
+            }
+        }
+
+        if (!empty($routeExists) && empty($runRoute)) {
+
+            $this->error = Router::METHOD_NOT_ALLOWED;
+
+            if (!empty($this->fallback) && !empty($this->error)) {
+                $this->runFallback();
+            }
+
+            return;
         }
 
         if (!empty($runRoute)) {
+            if (!empty($runRoute['before']['handler'])) {
 
-            if (!empty($this->middlewares[$this->httpMethod])) {
-
-                foreach ($this->middlewares[$this->httpMethod] as $middleware => $params) {
-                    if (preg_match("~^" . trim($middleware, '/') . "$~", $this->requestRoute)) {
-                        $runMiddleware = $params;
-                    }
-                }
-
-                if (!empty($runMiddleware)) {
-
-                    $middleware = $runMiddleware['handler'];
-                    $method = $runMiddleware['action'] ?? null;
-
-                    if (is_callable($middleware)) {
-                        call_user_func($middleware);
-                    } else {
-                        if (class_exists($middleware)) {
-                            $middleware = new $middleware($this);
-
-                            if (method_exists($middleware, $method)) {
-                                $middleware->$method();
-                            } else {
-                                $this->error = Router::NOT_IMPLEMENTED;
-                                return;
-                            }
-                        }
-                    }
-                }
+                $this->dispatch($runRoute['before']['handler'], $runRoute['before']['action']);
             }
 
-            if (is_callable($runRoute['handler'])) {
-                call_user_func($runRoute['handler'], ($runRoute['data'] ?? []));
-                return;
+            $this->dispatch($runRoute['handler'], $runRoute['action'], $runRoute['data']);
+
+            if (!empty($runRoute['after']['handler'])) {
+
+                $this->dispatch($runRoute['after']['handler'], $runRoute['after']['action']);
             }
 
-            $controller = $runRoute['handler'];
-            $method = $runRoute['action'] ?? null;
-
-            if (class_exists($controller)) {
-                $newController = new $controller($this);
-
-                if (method_exists($controller, $method)) {
-                    $newController->$method(($runRoute['data'] ?? []));
-                } else {
-                    $this->error = Router::NOT_IMPLEMENTED;
-                }
-                return;
-            }
-
-            $this->error = Router::BAD_REQUEST;
             return;
         }
 
         $this->error = Router::NOT_FOUND;
-        return;
+
+        if (!empty($this->fallback) && !empty($this->error)) {
+            $this->runFallback();
+        }
     }
 
     /**
-     * @param string $route
+     * @param string|callable $handler
+     * @param string|null $action
      */
-    public function redirect($route)
+    private function dispatch($handler, $action = null, $data = [])
     {
-        $router = trim($route, '/');
+        if (is_callable($handler)) {
+            call_user_func($handler, $data);
+            return;
+        }
 
-        header("Location: {$this->rootURL}/{$router}");
+        if (class_exists($handler)) {
+
+            $newHandler = new $handler($this);
+
+            if (method_exists($handler, $action)) {
+
+                $newHandler->$action($data);
+            } else {
+
+                $this->error = Router::NOT_IMPLEMENTED;
+            }
+        } else {
+
+            $this->error = Router::NOT_IMPLEMENTED;
+        }
+
+        if (!empty($this->fallback) && !empty($this->error)) {
+            $this->runFallback();
+        }
+    }
+
+    public function fallback($handler)
+    {
+        if (is_string($handler)) {
+            $this->fallback['handler'] = explode(":", $handler)[0];
+            $this->fallback['action'] = explode(":", $handler)[1] ?? null;
+            return;
+        }
+
+        $this->fallback['handler'] = $handler;
+        $this->fallback['action'] = null;
+    }
+
+    public function runFallback()
+    {
+        if (is_string($this->fallback['handler']) && !class_exists($this->fallback['handler'])) {
+            http_response_code(500);
+            throw new \Exception("Error Processing Fallback Class", 500);
+        }
+
+        http_response_code($this->error ?? '404');
+        $this->dispatch($this->fallback['handler'], $this->fallback['action'], ['error' => $this->error ?? '404']);
         exit;
     }
 
-    /**
-     * @return int|null
-     */
-    public function error()
+    public function redirect()
     {
-        return $this->error;
+        //
     }
 
-    /**
-     * @return string|array|null
-     */
-    public function routes()
+    public function route()
     {
-        return $this->routes;
+        //
     }
 }
